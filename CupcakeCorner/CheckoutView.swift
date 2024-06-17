1b454e607a2676df85f9509b5ba861e560f45259
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-17 17:45:51 +0000

    edited address view and, added address validation

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,19 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Yashavika Singh on 17.06.24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order = Order()
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order() )
+}
