d7e9627e2a2a99ca472c7dc4e3d050b098c8bac4
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-17 12:06:07 +0000

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Yashavika Singh on 17.06.24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
