18d37225c284586c6e097540f3fbe3d38d5819f1
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-17 19:17:24 +0000

    added checkout view and cost in order

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -10,10 +10,34 @@ import SwiftUI
 struct CheckoutView: View {
     var order = Order()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+       ScrollView
+        {
+            VStack{
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3){
+                    image in image.resizable().scaledToFit()
+                } placeholder: {
+                 ProgressView()
+                }
+                .frame(height: 233 )
+                    
+                
+                Text("Your total cost is: \(order.cost, format: .currency(code: "USD"))")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                
+                Button("Place Order ", action: {}).padding()
+                
+            }
+          
+        }
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+      
     }
 }
 
 #Preview {
     CheckoutView(order: Order() )
 }
+
--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -11,7 +11,7 @@ import Foundation
 @Observable
 class Order{
     
-    static let types = ["Strawberry", "Vanilla", "Chocolate", "Rainbow"]
+    static let types = ["Vanilla","Strawberry",  "Chocolate", "Rainbow"]
     var type = 0
     var quantity = 0
     var specialRequestEnabled = false
@@ -38,5 +38,25 @@ class Order{
         if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {return false}
           return true
     }
+    
+    
+    var cost: Decimal{
+        // 2 dollars per cake
+        var cost = Decimal(quantity)*2
+        // extra for fancier cake
+        cost += Decimal(type)/2
+        // 
+         if(extraFrosting)
+            { cost += Decimal(quantity) }
+        
+        
+        if(extraFrosting)
+           { cost += Decimal(quantity)/2  }
+        
+        return cost
+        
+    }
+    
+    
     
 }
